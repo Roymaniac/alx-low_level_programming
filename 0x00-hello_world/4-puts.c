ab4efaea0cdc28d710589910859a248b61e51316
Roymaniac <amolojoseph2020@gmail.com>
2022-03-10 05:53:53 -0800

    Script for task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -4,7 +4,6 @@
  * * main - prints the phrase "Programming is like building a multilingual puzzle
  * * Return: 0 if exited properly, non-zero otherwise
  * */
-
 int main(void) 
 {
        puts("\"Programming is like building a multilingual puzzle");
