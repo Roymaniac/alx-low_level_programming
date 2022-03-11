a9ee78ddffdb1e700d12abd827b16d8759ad8aaf
Roymaniac <amolojoseph2020@gmail.com>
2022-03-11 10:56:14 -0800

    Script for task 4

---
+++ 0x01-variables_if_else_while/4-alphabt.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - prints aplhabet in lowercase,
+ * followed by a new line, except q and e
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char ch = 'a';
+
+	while (ch <= 'z')
+	{
+		if (ch != 'e' && ch != 'q')
+		{
+			putchar(ch);
+		}
+		ch++
+	}
+	putchar('\n');
+	return (0);
+}
