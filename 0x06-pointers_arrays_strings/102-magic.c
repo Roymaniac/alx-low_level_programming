b94155a254a210cfc143d5b0afa5049accefb65d
Roymaniac <amolojoseph2020@gmail.com>
2022-03-24 17:30:26 -0700

    Task 14-15

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -1,28 +1,17 @@
+#include "main.h"
 #include <stdio.h>
 
-/*
- * main - write your line of code
- * return: 0
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
  */
 int main(void)
 {
-	int n;
-	int a[5];
-	int *p;
+	char buffer[] = 
 
-	a[2] = 1024;
-	p = &n;
-
-	/*
-   * write your line of code here...
-   * Remember:
-   * - you are not allowed to use a
-   * - you are not allowed to modify p
-   * - only one statement
-   * - you are not allowed to code anything else than this line of code
-   */
-	*(p + 5) = 98;
-	/* ...so that this prints 98\n */
-	printf("a[2] = %d\n", a[2]);
+	printf("%s\n", buffer);
+	printf("---------------------------------\n");
+	print_buffer(buffer, sizeof(buffer));
 	return (0);
 }
