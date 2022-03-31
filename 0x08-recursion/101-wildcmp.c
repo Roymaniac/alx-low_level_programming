b1fe69d8d712bd62b1719e5f5aaf0b21db76fcff
Roymaniac <amolojoseph2020@gmail.com>
2022-03-30 19:53:15 -0700

    fixes

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -6,20 +6,22 @@
  **/
 int wildcmp(char *s1, char *s2)
 {
-	if (*s1 == '\0' && *s2 == '\0')
-		return (1);
-	if (*s1 == *s2)
-		return (wildcmp(s1 + 1, s2 + 1));
-	if (*s2 == '\0')
-		return (0);
-	if (*s2 == '*')
-		return (wildcmp(s1 + 1, s2) || wildcmp(s1, s2 + 1));
 	if (*s1 == '\0')
 	{
-		if (*s2 != '*')
-			return (0);
-		else
+		if (*s2 != '\0' && *s2 == '*')
+		{
 			return (wildcmp(s1, s2 + 1));
+		}
+		return (*s2 == '\0');
+	}
+
+	if (*s2 == '*')
+	{
+		return (wildcmp(s1 + 1, s2) || wildcmp(s1, s2 + 1));
+	}
+	else if (*s1 == *s2)
+	{
+		return (wildcmp(s1 + 1, s2 + 1));
 	}
 	return (0);
 }
